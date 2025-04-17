4c1cd3dbe0cab3108453bca507e77dcf75d7208b
phyrozz <joshamalabanan70@gmail.com>
2025-04-17 15:24:29 +0800

    removed example files

--- src/endpoints/post.ts
+++ src/endpoints/post.ts
@@ -1,6 +1,7 @@
 import { OpenAPIRoute } from "chanfana";
 import { z } from "zod";
 import { nanoid } from "nanoid";
+import OpenAI from "openai";
 
 export class Post extends OpenAPIRoute {
   schema = {
@@ -56,7 +57,21 @@ export class Post extends OpenAPIRoute {
     },
   };
 
+  arrayBufferToBase64(buffer: any) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  }
+
   async handle(c) {
+    let openai = new OpenAI({
+      apiKey: c.env.OPENAI_KEY,
+    });
+
     const formData = await c.req.parseBody();
     const file = formData["file"];
 
@@ -71,11 +86,82 @@ export class Post extends OpenAPIRoute {
       },
     });
 
+    const object = await c.env.UPLOADS_BUCKET.get(key);
+    if (!object || !object.body) {
+      return c.json({ success: false, error: "Image not found" }, 404);
+    }
+
+    const arrayBuffer = await new Response(object.body).arrayBuffer();
+    const base64Image = this.arrayBufferToBase64(arrayBuffer);
+
+    // Call OpenAI API to read the image and generate a response
+    const response = await openai.responses.create({
+      model: "gpt-4.1-mini",
+      input: [
+        {
+          "role": "system",
+          "content": [
+            {
+              "type": "input_text",
+              "text": "you are a food identifier bot. you must accurately identify the food on the image sent by the user."
+            }
+          ]
+        },
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "input_image",
+              "image_url": `data:${file.type};base64,${base64Image}`, // The image key from R2
+              "detail": "auto"
+            },
+            {
+              "type": "input_text",
+              "text": "what is this?"
+            }
+          ]
+        }
+      ],
+      text: {
+        "format": {
+          "type": "json_schema",
+          "name": "food_schema",
+          "strict": true,
+          "schema": {
+            "type": "object",
+            "properties": {
+              "food": {
+                "type": "string",
+                "enum": ["Hotdog", "Not Hotdog"],
+                "description": "The name of the food item. Can be either 'Hotdog' or 'Not Hotdog'."
+              },
+              "description": {
+                "type": "string",
+                "description": "A description of the food item."
+              }
+            },
+            "required": [
+              "food",
+              "description"
+            ],
+            "additionalProperties": false
+          }
+        }
+      },
+      reasoning: {},
+      tools: [],
+      temperature: 1,
+      max_output_tokens: 2048,
+      top_p: 1,
+      store: true
+    });
+
     return c.json({
       success: true,
       result: {
         key,
-      },
+        body: JSON.parse(response.output_text)
+      }
     });
   }
 }
